7d2d549a48a733a19c1610b95e5eae5496a65532
Christian Hammers <ch@lathspell.de>
2012-07-15 02:42:04 +0200

    added cli

--- main/src/de/lathspell/dtx2gp/frontend/Cli.kt
+++ main/src/de/lathspell/dtx2gp/frontend/Cli.kt
@@ -1,6 +1,36 @@
 package de.lathspell.dtx2gp.frontend
 
-/** CLI Frontend */
-public class Cli {
+import de.lathspell.dtx2gp.DtxToTuxguitar
+import de.lathspell.dtx2gp.dtx.DTXFile
+import de.lathspell.dtx2gp.dtx.DTXReader
+import de.lathspell.dtx2gp.tg.TGHelper
+import java.io.File
+import org.apache.log4j.ConsoleAppender
+import org.apache.log4j.Logger
+import org.apache.log4j.PatternLayout
+import org.herac.tuxguitar.song.models.TGSong
 
+/** CLI Frontend */
+fun main(args : Array<String>) {
+    if (args.size != 2) {
+        println("Usage: dtx2gp <input> <output>\n")
+        System.exit(1)
+    }
+    val fnameIn = args[0]
+    val fnameOut = args[1]
+
+    val log = Logger.getRootLogger()!!
+    log.addAppender(ConsoleAppender(PatternLayout(PatternLayout.TTCC_CONVERSION_PATTERN)))
+
+    log.info("Loading $fnameIn")
+    val text = File(fnameIn).readText("UTF-8")
+
+    log.info("Converting...")
+    val dtxfile: DTXFile = DTXReader().readText(text)
+    val tgsong: TGSong = DtxToTuxguitar().convertSong(dtxfile)
+
+    log.info("Writing $fnameOut")
+    TGHelper().writeGP5Song(fnameOut, tgsong)
+
+    log.info("Finished.");
 }
